6bd1175f8fbd3f7c2b43814f4b58f6ebd8f5309a
Leopold TRIPOT <leopold.tripot@gmail.com>
2024-05-31 11:35:50 +0200

    ok

---
+++ app.py
@@ -0,0 +1,15 @@
+import streamlit as st
+import streamlit.components.v1 as components
+
+st.title("Score 10 000 To see my prediction :)")
+
+# Path to the HTML file
+html_file = "pong.html"
+
+# Read the HTML file
+with open(html_file, 'r') as f:
+    html_code = f.read()
+# Embed the HTML code in the Streamlit app
+components.html(html_code, height=600, width=800)
+
+st.title("Use Space to play - maybe u need to click on game before :/")
