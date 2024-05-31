f2ba430f2a7b651d71f85351ae6de31ed998aef6
Leopold TRIPOT <leopold.tripot@gmail.com>
2024-05-31 12:03:47 +0200

    ok

--- app.py
+++ app.py
@@ -10,6 +10,6 @@ html_file = "pong.html"
 with open(html_file, 'r') as f:
     html_code = f.read()
 # Embed the HTML code in the Streamlit app
-components.html(html_code, height=600, width=800)
+components.html(html_code, height=800, width=1000)
 
 st.title("Use Space to play - maybe u need to click on game before :/")
